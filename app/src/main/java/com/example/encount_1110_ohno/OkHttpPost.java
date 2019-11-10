e33bd32bd0e7771286a93ee08c60ec08431e4018
fjb-ohnokinako-3G <x17g006@chiba-fjb.ac.jp>
2019-11-11 00:28:17 +0900

    ok

---
+++ app/src/main/java/com/example/encount_1110_ohno/OkHttpPost.java
@@ -0,0 +1,65 @@
+package com.example.encount_1110_ohno;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class OkHttpPost extends AsyncTask<String,String,String> {
+
+    //public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    //String json = "{\"name\":\"名前\", \"taxis\":\"分類\"}";
+
+    public static String cmnt = "コメントです";
+    //String cmnt1 = MainActivity.cmnt;
+    public static String log1 = "";
+
+    //public static String aaa = "aa";
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://kinako.cf/api/pass_check.php";
+
+        //Map<String, String> formParamMap = new HashMap<>();
+        //formParamMap.put("word", "abc");
+        final FormBody.Builder formBuilder = new FormBody.Builder();
+        //formParamMap.forEach(formBuilder::add);
+        formBuilder.add("word", cmnt);
+        RequestBody body = formBuilder.build();
+
+        //RequestBody body = RequestBody.create(JSON, json);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            //aaa = response.body().string();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String str) {
+        Log.d("Debug",str);
+    }
+}
